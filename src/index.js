6fd769c32c0b0ae17a5170c6547ea0ab5a8a0a22
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-04-06 20:07:12 +0300

    add simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 import './css/styles.css';
 import { renderPictures } from './pictures';
 import PicturesApiServise from './fetchPictures';
 import LoadMoreBtn from './loadMoreBtn';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
-// import axios from 'axios';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import { lightbox } from './litebox';
 
 
 const refs = {
@@ -21,6 +21,7 @@ const loadMoreBtn = new LoadMoreBtn({
 });
 
 refs.searchForm.addEventListener('submit', onSearch);
+
 loadMoreBtn.refs.button.addEventListener('click', feachHits);
 // loadMoreBtn.disable() 
 
@@ -36,6 +37,7 @@ function onSearch(event) {
     loadMoreBtn.show()
     picturesApiServise.resetPage();
     feachHits()
+     
    picturesApiServise.fetchArticles().then(data => { 
         const totalHits = data.totalHits;
         return Notify.info(`Hooray! We found ${totalHits} images.`)     
@@ -58,6 +60,7 @@ function feachHits(){
         }
       
         renderPictures(hits);
+         lightbox.refresh();
     
         // loadMoreBtn.enable();
         if (hits.length === 0) {
@@ -67,13 +70,3 @@ function feachHits(){
     })
    
 }
-
-
-// let lightbox = new SimpleLightbox('.photo-card a', {
-//     captionsData: 'alt',
-//     captionDelay: 250,
-//     enableKeyboard: "Escape",
-// });
-
-
-//   lightbox.refresh();
